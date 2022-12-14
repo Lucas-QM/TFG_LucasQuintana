2987fca2f48faba11526f7fc0c8ee51a6cbbd3c0
Lucas-QM <lucasquimon@gmail.com>
2022-12-14 19:04:48 +0100

    Created MainMenu
    Main Menu with 3 buttons, start game, options and quit game

---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        print("Game closed");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowSettings()
+    {
+        anim.SetBool("ShowSettings", true);
+    }
+
+    public void HideSettings()
+    {
+        anim.SetBool("ShowSettings", false);
+    }
+}
